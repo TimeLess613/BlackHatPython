a63a5db22b2e8526c4706d053789508e2f444e14
TimeLess <mellozh@gmail.com>
2022-09-14 00:05:52 +0900

    参考资料

--- 2_network_programming/README.md
+++ 2_network_programming/README.md
@@ -1,2 +1,6 @@
 # 网络编程
 参考资料：https://www.liaoxuefeng.com/wiki/1016959663602400/1017787560490144
+
+## 相关知识： 
+报头格式 
+编码：http://c.biancheng.net/view/4305.html
--- 2_network_programming/tcp_client.py
+++ 2_network_programming/tcp_client.py
@@ -1,22 +1,48 @@
 import socket
 
-def connect(host="www.google.com", port=80):
-    # object
+def connect(host, port):
+    # socket object
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     # connect
     client.connect((host, port))
-
     # sent data
-    client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+    ## 相关知识：报头格式，编码
+    client.send(b"GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n")
 
     # receive data
-    response = client.recv(4096)
-
+    ## 仅接收了一次。每次最多接收1KB
+    response = client.recv(1024)
     print(response.decode())
+
+    ## 全接收需要while循环
+    ## 但似乎HTTP/1.1默认"Connection:Keep-Alive"，
+    ## 如果报头不加"Connection: close"，则下面的while循环不会立马结束。
+    buffer = []
+    i = 0
+    while True:
+        i = i + 1
+
+        d = client.recv(1024)
+        if d:
+            print(i)
+            buffer.append(d)
+        else:
+            break
+    data = b''.join(buffer)
+    print(data.decode())
+
+
     client.close()
+    # save_html(data)
+
+
+def save_html(data):
+    header, html = data.split(b'\r\n\r\n', 1)
+    print(header.decode('utf-8'))
+    with open('google.html', 'wb') as f:
+        f.write(html)
 
 if __name__ == '__main__':
-    target_host = "www.google.com"
-    target_port = 80
-    connect(target_host, target_port)
+    host = "www.google.com"
+    port = 80
+    connect(host, port)
