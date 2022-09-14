c16a46ebec1628acb11ebe650da6429188527212
TimeLess <mellozh@gmail.com>
2022-09-14 23:21:31 +0900

    tcp fine

--- 2_network_programming/tcp_client.py
+++ 2_network_programming/tcp_client.py
@@ -1,29 +1,30 @@
 import socket
 
-def connect(host, port):
-    # socket object
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def tcp_client(host, port):
+    # socket object with IPv4 and TCP
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # connect
-    client.connect((host, port))
+    s.connect((host, port))
+
     # sent data
     ## 相关知识：报头格式，编码
-    client.send(b"GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n")
+    s.send(b"GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n")
 
     # receive data
-    ## 仅接收了一次。每次最多接收1KB
-    response = client.recv(1024)
+    ## 这里仅接收了一次。每次最多接收1KB
+    response = s.recv(1024)
     print(response.decode())
 
     ## 全接收需要while循环
     ## 但似乎HTTP/1.1默认"Connection:Keep-Alive"，
     ## 如果报头不加"Connection: close"，则下面的while循环不会立马结束。
-    ## buffer接收每一个包，最后拼接到data。
+    ## buffer里append接收到的每一个包，最后拼接到data。
     buffer = []
     i = 0
     while True:
         i = i + 1
 
-        d = client.recv(1024)
+        d = s.recv(1024)
         if d:
             print(i)
             buffer.append(d)
@@ -32,8 +33,7 @@ def connect(host, port):
     data = b''.join(buffer)
     print(data.decode())
 
-
-    client.close()
+    s.close()
     # save_html(data)
 
 
@@ -46,6 +46,8 @@ def save_html(data):
         f.write(html)
 
 if __name__ == '__main__':
-    host = "www.google.com"
-    port = 80
-    connect(host, port)
+    # host = "www.google.com"
+    # port = 80
+    host = "127.0.0.1"
+    port = 4444
+    tcp_client(host, port)
--- 2_network_programming/tcp_server.py
+++ 2_network_programming/tcp_server.py
@@ -1,23 +1,19 @@
 import socket
 import threading
+import time
 
-def main():
-    bind_ip = "0.0.0.0"
-    bind_port = 4444
-    
+def tcp_server(ip, port):
     # socket object with IPv4 and TCP
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # listener，max connection 5.
-    server.bind((bind_ip, bind_port))
-    server.listen(5)
+    s.bind((bind_ip, bind_port))    
+    s.listen(5)
     print(f"[*] Listening on {bind_ip}:{bind_port}")
-    # print "[*] Listening on %s:%d" % (bind_ip, bind_port)
 
     while True:
         # wait for connection, save the object client to var client
-        ## accept()会等待并返回一个客户端的连接
-        client, addr = server.accept()
-        print(f"[*] Accepted connection from: {addr[0]}:{addr[1]}")
+        ## accept()会等待并返回一个客户端的连接（tuple）
+        client, addr = s.accept()
 
         # creat new thread, handle_client().
         # spin up our client thread to handle/receive incoming data
@@ -27,15 +23,28 @@ def main():
 # this is our client handling thread
 ## 由于会有多个客户端访问，所以每个连接都需要一个新的进程/线程来处理，否则，服务器一次就只能服务一个客户端了。
 def handle_client(client_socket, addr):
-    with client_socket as sock:
-        # 似乎又是只接收一次。
-        request = sock.recv(1024)
+    print(f"[*] Accepted connection from: {addr[0]}:{addr[1]}")
 
+    # 似乎又是只接收一次。接收完就关闭当前线程
+    with client_socket as sock:
+        request = sock.recv(1024)
         # print out what the client sends
-        print(f"[*] Received: {request.decode('utf-8')}")
+        print(f"[*] Received: \r\n{request.decode('utf-8')}")
         # send back a packet
         sock.send(b'ACK')
 
+    # # 全接收的循环？
+    # while True:
+    #     data = client_socket.recv(1024)
+    #     print(f"[*] Received: \r\n{data.decode('utf-8')}")
+    #     client_socket.send(b'ACK')
+    #     time.sleep(1)
+    #     if not data or data.decode('utf-8')=='exit':
+    #         break
+    # client_socket.close()
+
 
 if __name__ == "__main__":
-    main()
+    bind_ip = "127.0.0.1"
+    bind_port = 4444
+    tcp_server(bind_ip, bind_port)
