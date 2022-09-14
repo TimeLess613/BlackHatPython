33acf9e2a4c93c337db8f13039de8d337377fc9a
TimeLess <mellozh@gmail.com>
2022-09-15 00:09:20 +0900

    new file

---
+++ 2_network_programming/netcat.py
@@ -0,0 +1,17 @@
+import argparse
+import socket
+import shlex
+import subprocess
+import sys
+import textwrap
+import threading
+
+def tcp_client(host, port):
+    
+
+if __name__ == '__main__':
+    # host = "www.google.com"
+    # port = 80
+    host = "127.0.0.1"
+    port = 4444
+    tcp_client(host, port)
